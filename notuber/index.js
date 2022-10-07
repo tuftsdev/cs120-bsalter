627a58e0866107446f6c9a865ba076fb9ffd2aeb
Benn Salter <benn@heynarus.com>
2022-10-07 13:47:43 -0400

    set up basic files and map

---
+++ notuber/index.js
@@ -0,0 +1,10 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 42.352271, lng: -71.05524200000001 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
